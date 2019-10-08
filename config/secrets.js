b77143d6cf0fcefbaee7c507a7b94dafbb4fd98b
Nico Sosa <nico.sosa93@gmail.com>
2019-10-08 09:19:02 -0300

    Delete secrets.js

--- config/secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  cloudinary:{
-    api_key: '652118227547474',
-    cloud_name: 'nicososa',
-    api_secret: 's1z2j953I38wGu7YUweBaxTG9zA'
-  },
-  jwtSecret:'s1z2j953I38wGu7YUweBaxTG9zA'
-};
